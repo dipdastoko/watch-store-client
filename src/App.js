35d22c78e03c7c090f9910223f3e7453547418f8
dipdastoko <dipdastoko@gmail.com>
2022-09-01 04:09:13 +0600

    dashboard component created

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
 import AuthProvider from './Context/AuthProvider';
+import Dashboard from './Pages/Dasboard/Dashboard';
 import Explore from './Pages/Explore/Explore';
 import Home from './Pages/Home/Home/Home';
 import Login from './Pages/Login/Login/Login';
@@ -16,6 +17,7 @@ function App() {
             <Route path='explore' element={<Explore />} />
             <Route path='login' element={<Login />} />
             <Route path='register' element={<Register />} />
+            <Route path='dashboard' element={<Dashboard />} />
           </Routes>
         </BrowserRouter>
       </AuthProvider>
