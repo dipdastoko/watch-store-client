5fd14727a700498957e9a570361de168d66880c7
dipdastoko <dipdastoko@gmail.com>
2022-09-06 02:34:15 +0600

    review stored in database

--- src/Pages/Dasboard/TakeReview/TakeReview.js
+++ src/Pages/Dasboard/TakeReview/TakeReview.js
@@ -1,10 +1,96 @@
+import { Alert, Button, Collapse, Rating, TextField, Typography } from '@mui/material';
+import { Box } from '@mui/system';
 import React from 'react';
+import { useState } from 'react';
+import useAuth from '../../../Hooks/useAuth';
 
 const TakeReview = () => {
+    const [value, setValue] = useState(0);
+    const [reviewText, setReviewText] = useState();
+    const [isSuccess, setIsSuccess] = useState('');
+    const [open, setOpen] = useState(true);
+
+    const date = new Date();
+
+    const { user } = useAuth();
+
+    const handleOnChange = e => {
+        setReviewText(e.target.value);
+    };
+
+    const handleSubmitReview = e => {
+        setOpen(true);
+
+        const review = { reviewText, rating: value, name: user.displayName, date: date.toLocaleDateString() };
+        console.log(review);
+
+        fetch('http://localhost:5000/review', {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(review)
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+                if (data.insertedId) {
+                    setIsSuccess('success');
+                    setReviewText('');
+                    setValue(0);
+                }
+                else {
+                    setIsSuccess('error');
+                }
+            });
+        e.preventDefault();
+    };
+
     return (
-        <div>
-            <h2>Take Review</h2>
-        </div>
+        <Box>
+            <Typography variant='h5'>Give Your Review</Typography>
+            <form onSubmit={handleSubmitReview}>
+                <TextField
+                    required
+                    label="Review"
+                    onChange={handleOnChange}
+                    value={reviewText}
+                    multiline
+                    fullWidth
+                    rows={6}
+                />
+                <br />
+                <br />
+                Rate Us:
+                <br />
+                <Rating
+                    name="simple-controlled"
+                    value={value}
+                    onChange={(event, newValue) => {
+                        setValue(newValue);
+                    }}
+                />
+                <br /><br />
+                <Button type='submit' variant='contained'>Submit Review</Button>
+            </form>
+            <br />
+            {
+                isSuccess === 'success' &&
+                <Collapse in={open}>
+                    <Alert onClose={() => { setOpen(false) }} variant="filled" severity="success">Your Review Submitted Successfully!</Alert>
+                </Collapse>
+
+
+            }
+            {
+                isSuccess === 'error' &&
+                <Collapse in={open}>
+                    <Alert onClose={() => { setOpen(false) }} variant="filled" severity="success">Failed to Submit Your Review!</Alert>
+                </Collapse>
+
+
+            }
+        </Box>
     );
 };
 
